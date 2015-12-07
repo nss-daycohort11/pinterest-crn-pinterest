fc47862f197d0cbbddca6b42939a4f62d20942d7
Red French <french@volstate.net>
2015-12-07 15:03:15 -0600

    new branch

--- app/Controllers/ctrl1.js
+++ app/Controllers/ctrl1.js
@@ -1,6 +1,45 @@
-app.controller("ctrl1", ["$q", "$http", "$scope", "$firebaseArray", function($q, $http, $scope, $firebaseArray) {
-	
-  
-  
+// app.controller("ctrl1", ["$q", "$http", "$scope", "$firebaseArray", 
+// 	function($q, $http, $scope, $firebaseArray) {
 
-}]);
+// let's create a re-usable factory that generates the $firebaseAuth instance
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://crn-pinterest.firebaseio.com/");
+    return $firebaseAuth(ref);
+  }
+]);
+
+// and use it in our controller
+app.controller("ctrl1", ["$scope", "Auth",
+  function($scope, Auth) {
+    $scope.createUser = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      Auth.$createUser({
+        email: $scope.email,
+        password: $scope.password
+      }).then(function(userData) {
+        $scope.message = "User created with uid: " + userData.uid;
+      }).catch(function(error) {
+        $scope.error = error;
+      });
+    };
+
+    $scope.removeUser = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      Auth.$removeUser({
+        email: $scope.email,
+        password: $scope.password
+      }).then(function() {
+        $scope.message = "User removed";
+      }).catch(function(error) {
+        $scope.error = error;
+      });
+    };
+  }]);
+
+// }]);
+
--- app/app.js
+++ app/app.js
@@ -2,6 +2,11 @@ var app = angular.module("app", ['ngRoute', 'firebase', 'angular.filter']);
 
 app.config(['$routeProvider',
   function($routeProvider) {
+   $routeProvider
+   .when('/login', {  // when URL ends with this
+       templateUrl: 'Partials/login.html',  // location of template
+       controller: 'ctrl1.js'  // name of controller (songCtrl.js)
+     })
+     .otherwise({ redirectTo: '/login' });
+ }]);
 
-    
-  }]);
